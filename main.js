1e91587e82ed943afb55674e9a2bc58196186f75
Rafael D' Santos <117246770+Rafasennin@users.noreply.github.com>
2023-03-04 13:44:46 -0300

    Projeto finalizado

---
+++ main.js
@@ -0,0 +1,57 @@
+
+
+const tipAmount = document.getElementById("amount");
+const teclas = document.querySelectorAll(".tecla__tecla");
+const inputValue = document.getElementById("input__number");
+const customInput = document.getElementById("tecla__number");
+const people = document.getElementById("people__number");
+const totalPeople = document.getElementById("total");
+const numberZero = document.getElementById("number__zero");
+
+
+teclas[0].addEventListener("click", ()=>{
+    tipAmount.innerHTML =   parseFloat((inputValue.value/100 ) * 5).toFixed(2);
+})
+
+teclas[1].addEventListener("click", ()=>{
+    tipAmount.innerHTML =   parseFloat((inputValue.value/100 ) * 10).toFixed(2);
+})
+
+teclas[2].addEventListener("click", ()=>{
+    tipAmount.innerHTML =   parseFloat((inputValue.value/100 ) * 15).toFixed(2);
+})
+
+teclas[3].addEventListener("click", ()=>{
+    tipAmount.innerHTML =   parseFloat((inputValue.value/100 ) * 25).toFixed(2);
+})
+
+teclas[4].addEventListener("click", ()=>{
+    tipAmount.innerHTML =   parseFloat((inputValue.value/100 ) * 50).toFixed(2);
+})
+
+customInput.addEventListener("keyup", ()=>{
+    tipAmount.innerHTML =   parseFloat((inputValue.value/100 ) * customInput.value).toFixed(2);
+})
+
+
+
+    numberZero.innerHTML = " O número de pessoas não pode ser zero '0'";
+    totalPeople.innerHTML = "R$ 0.00";
+
+
+people.addEventListener("keyup", ()=>{
+    totalPeople.innerHTML =   parseFloat(tipAmount.innerText / people.value).toFixed(2);
+    numberZero.innerHTML = "";
+})
+
+
+
+
+
+   
+
+
+
+function resetTip(){
+    window.location.reload();
+}
