2d132ed827fb17b79e6604d29dc6feca01202539
Rafael Dos Santos <rafasennin@hotmail.com>
2023-03-04 08:40:15 -0300

    Adicionando funcionaliddae a aplicação com Javascript

---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+const tipAmount = document.getElementById("amount");
+const teclas = document.querySelectorAll(".tecla__tecla");
+const inputValue = document.getElementById("input__number");
+const customInput = document.getElementById("tecla__number");
+const people = document.getElementById("people__number");
+const totalPeople = document.getElementById("total");
+
+
+teclas[0].addEventListener("click", ()=>{
+    tipAmount.innerHTML = "R$ " + parseFloat((inputValue.value/100 ) * 5).toFixed(2);
+})
+
+teclas[1].addEventListener("click", ()=>{
+    tipAmount.innerHTML = "R$ " + parseFloat((inputValue.value/100 ) * 10).toFixed(2);
+})
+
+teclas[2].addEventListener("click", ()=>{
+    tipAmount.innerHTML = "R$ " + parseFloat((inputValue.value/100 ) * 15).toFixed(2);
+})
+
+teclas[3].addEventListener("click", ()=>{
+    tipAmount.innerHTML = "R$ " + parseFloat((inputValue.value/100 ) * 25).toFixed(2);
+})
+
+teclas[4].addEventListener("click", ()=>{
+    tipAmount.innerHTML = "R$ " + parseFloat((inputValue.value/100 ) * 50).toFixed(2);
+})
+
+customInput.addEventListener("click", ()=>{
+    tipAmount.innerHTML = "R$ " + parseFloat((inputValue.value/100 ) * customInput.value).toFixed(2);
+})
+
+people.addEventListener("click", ()=>{
+    totalPeople.innerHTML = "R$ " + parseFloat(tipAmount.innerText / people.value).toFixed(2);
+})
+
+
+
+
+   
+
+
+
+function resetTip(){
+    window.location.reload();
+}
